50901e2ad780976d06bfbd84ffaa04ac82956091
Tushar_Bohra <87470105+TusharBohra@users.noreply.github.com>
2023-01-07 23:55:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,50 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+PrintDeveloperswithMap()
+PrintDeveloperbyForEach()
+addData()
+concatenateArray()
+removeAdmin()
+
 function PrintDeveloperswithMap() {
-  //Write your code here
+  arr.map((item)=>{ 
+    if(item.profession == "developer"){
+        console.log(item.name)
+    }
+        
+})
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+    arr.forEach(function printDev(e) {
+        if(e.profession == "developer"){
+            console.log(e.name)
+  }
+})
 }
+  
 
 function addData() {
-  //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  console.log(arr)
 }
 
 function removeAdmin() {
-  //Write your code here
+  arr.forEach(function printDev(e) {
+        if(e.profession == "developer"){
+            arr.pop()
+  }
+})
+console.log(arr)
 }
 
 function concatenateArray() {
-  //Write your code here
+  let arr1 = [
+  { id: 5, name: "Jet", age: "19", profession: "developer" },
+  { id: 6, name: "Jay", age: "24", profession: "admin" },
+  { id: 7, name: "Pope", age: "17", profession: "intern" },
+];
+const result = arr.concat(arr1)
+console.log(result)
 }
